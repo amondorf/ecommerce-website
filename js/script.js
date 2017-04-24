17867b8bdfb2a4be4b27dca6031e9622907b70ac
Agnes Mondorf <Agnes@Agness-Air.fritz.box>
2017-04-24 09:41:25 +0200

    add continue reading link in mobile version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $('button').click(function(){
   $('.toggle').toggleClass('hide-blp');
 });
+
+ if ( $(window).width() < 739) {
+   $(document).ready(function() {
+   	var showText = 100;
+   	$('.book-description').each(function() {
+   		var content = $(this).html();
+
+   		if(content.length > showText) {
+   			var c = content.substr(0, showText);
+         var bookLink = $(this).attr("id");
+   			// var h = content.substr(showText-1, content.length - showText)
+   			var html = c + '<a href="' + bookLink + '">' + " ...continue reading" + "</a>";
+   			$(this).html(html);
+   		};
+   	});
+   });
+ }
