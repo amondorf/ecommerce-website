2f72cdc089e672130433e684c7ee59e6c28f778c
Agnes Mondorf <Agnes@Agness-Air.fritz.box>
2017-04-25 11:43:31 +0200

    add book details page

--- js/script.js
+++ js/script.js
@@ -18,3 +18,84 @@ $('button').click(function(){
    	});
    });
  }
+
+
+ // book site
+ var books = [{
+        id: "book1",
+         name: "Octopus Encounters1",
+         item: "test1",
+         author: "a",
+         description: "Detailed description of the book comes here.",
+         img: "img/example.png"
+
+     },
+     {
+       id: "book2",
+       name: "The Crimson Sign",
+       item: "test2",
+       author: "S. R. Keightley",
+       description: "In the year of grace 1689 men were not a whit more long-suffering nor more patient than they are to-day. The choleric captain who had been pacing the guard-room for a quarter of an hour showed evident signs that he was fast losing what temper he possessed. As he marched with a hasty stride up and down the oaken floor, and wheeled with military abruptness on the broad stone that formed the hearth, the rafters of black oak rang with the clank of his sword and the jingling of the spurs on his heavy jack-boots. He pulled with a gesture of impatience at the grizzled white moustache that concealed his mouth, and muttered anathemas which, had they been heard in the pious city of Londonderry, would have been deemed little in keeping with his reputation. Nor did he seem a man with whom others would take unwarrantable liberties, or keep dangling upon their careless will and pleasure.",
+       img: "img/the_crimson_sign.jpg"
+
+     },
+     {
+       id: "book3",
+       name: "Octopus Encounters3",
+       item: "test3",
+       author: "c",
+       description: "Detailed description of the book comes here.",
+       img: "img/example.png"
+
+     },
+     {
+       id: "book4",
+       name: "Octopus Encounters4",
+       item: "test4",
+       author: "d",
+       description: "Detailed description of the book comes here.",
+       img: "img/example.png"
+
+     },
+     {
+       id: "book5",
+       name: "Octopus Encounters5",
+       item: "test5",
+       author: "e",
+       description: "Detailed description of the book comes here.",
+       img: "img/example.png"
+     },
+     {
+       id: "book6",
+       name: "Octopus Encounters6",
+       item: "test6",
+       author: "f",
+       description: "Detailed description of the book comes here.",
+       img: "img/example.png"
+         }
+ ];
+
+ $('img').click(function(){
+  //  console.log("test");
+   var bookId = $(this).attr('id');
+   console.log(bookId);
+
+   index = books.findIndex(x => x.id==bookId);
+   console.log(index);
+
+   var name = books[index].name;
+   var author = books[index].author;
+   var image = books[index].img;
+   var description = books[index].description;
+   console.log(image);
+
+   $(".title").text(name);
+   $(".author").text("by " + author);
+   $(".details-descr").text(description);
+
+  document.getElementById("details-img").src= image;
+   console.log(name);
+   $("#myCarousel").addClass("hide-section");
+   $("#details-page").removeClass("hide-section");
+
+ });
